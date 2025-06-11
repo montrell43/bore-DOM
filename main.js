19e789df663d5d0fe06c2327da6186f9d292b706
kylexrose@gmail.com <kylexrose@gmail.com>
2025-06-11 09:19:37 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// 1. Alter the text color of the paragraph to a shade of aqua.
+
+
+
+// 2. Adjust the font size of the heading to 42 pixels.
+
+
+// 3. Replace the text in the paragraph with the new Lorem Ipsum content.
+
+// 4. Reduce the opacity of the 13th item to 30% transparency.
+
+// 5. Modify the text of the 3rd item to read "Greetings!"
+
+// 6. Update the image below the list to display the new image.
+
+// 7. Resize the new image to a height of 250 pixels.
+
+// 8. Integrate an additional image using innerHTML below the existing image.
+
+// 9. Adjust the height of the new image to 280 pixels programmatically.
+
+// 10. Append a 16th <li> element to the list using innerHTML.
+
+// 11. Assign the same class as the other list items to the new <li> element using JavaScript.
+
+// 12. Populate the new <li> with the text "Never gonna give you up."
